f6dfec0d9dae0b76fcbe0886671b4cb27a080dac
Greed <106820018+maxberglund2@users.noreply.github.com>
2023-12-09 20:19:14 +0100

    read all data + delete button
    video guided

---
+++ react-starter-kit/js/movies.js
@@ -0,0 +1,12 @@
+const Movies = [
+    {
+        title: "Batman",
+        year: 2022,
+        rating: 4.2
+    },
+    {
+        title: "Spiderman",
+        year: 2007,
+        rating: 4.8
+    }
+];
